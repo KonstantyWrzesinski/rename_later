b0308a34fbe3cada357cd3b8b4b19475df79cd6c
KonstantyWrzesinski <245050@student.uek.krakow.pl>
2026-03-16 20:50:54 +0100

    django

---
+++ rename_later/asgi.py
@@ -0,0 +1,16 @@
+"""
+ASGI config for rename_later project.
+
+It exposes the ASGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/asgi/
+"""
+
+import os
+
+from django.core.asgi import get_asgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rename_later.settings')
+
+application = get_asgi_application()
---
+++ rename_later/wsgi.py
@@ -0,0 +1,16 @@
+"""
+WSGI config for rename_later project.
+
+It exposes the WSGI callable as a module-level variable named ``application``.
+
+For more information on this file, see
+https://docs.djangoproject.com/en/6.0/howto/deployment/wsgi/
+"""
+
+import os
+
+from django.core.wsgi import get_wsgi_application
+
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'rename_later.settings')
+
+application = get_wsgi_application()
