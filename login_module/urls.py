bbe4fc593deab74bd027a1e312440faa8b0a0067
KonstantyWrzesinski <245050@student.uek.krakow.pl>
2026-04-26 00:45:34 +0200

    added login functionality

---
+++ login_module/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path, include
+from . import views
+from django.contrib.auth.views import LoginView
+
+urlpatterns = [
+   #path('', include('django.contrib.auth.urls')),
+   path('registration/',views.login, name = 'login'),
+    path('login/', views.sign_in, name='login'),
+    path('logout/', views.sign_out, name='logout'),
+    path('register/', views.sign_up, name='register'),
+]
--- rename_later/settings.py
+++ rename_later/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    "someapp"
+    "someapp",
+    "login_module"
 ]
 
 MIDDLEWARE = [
