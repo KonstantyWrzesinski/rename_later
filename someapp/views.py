ae96d312bb0afe128f3a943e6831faf6394b4392
Paul-Int <pawel.wilusz1334@gmail.com>
2026-05-25 10:27:04 +0200

    Finally redesigned homepage, God bless.

--- someapp/views.py
+++ someapp/views.py
@@ -3,12 +3,27 @@ from django.shortcuts import render
 from .models import TodoItem
 from .forms import CityForm
 from .services import get_current_weather, WeatherServiceError
-
+from django.core.cache import cache
 
 def home(request):
     form = CityForm(request.POST or None)
     weather = None
     error = None
+
+    popular_cities = cache.get("popular_cities")
+
+    if popular_cities is None:
+        popular_city_names = ["Warszawa", "Paris", "Barcelona", "Oslo"]
+        popular_cities = []
+
+        for city_name in popular_city_names:
+            try:
+                city_weather = get_current_weather(city_name)
+                popular_cities.append(city_weather)
+            except WeatherServiceError:
+                pass
+
+        cache.set("popular_cities", popular_cities, 300)  # Pobieranie Info z Api co 5 minut
 
     if request.method == "POST" and form.is_valid():
         city = form.cleaned_data["city"]
@@ -18,15 +33,12 @@ def home(request):
         except WeatherServiceError as exc:
             error = str(exc)
 
-    return render(
-        request,
-        "home.html",
-        {
-            "form": form,
-            "weather": weather,
-            "error": error,
-        },
-    )
+    return render(request, "home.html", {
+        "form": form,
+        "weather": weather,
+        "error": error,
+        "popular_cities": popular_cities,
+    })
 
 
 def todos(request):
