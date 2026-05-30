572e35b3ec78615dc010730fe5199e51ab665b4f
Paul-Int <pawel.wilusz1334@gmail.com>
2026-05-30 10:59:14 +0200

    Deleted the about path

--- someapp/urls.py
+++ someapp/urls.py
@@ -5,5 +5,4 @@ urlpatterns = [
     path("", views.home, name="home"),
     path("todos/", views.todos, name="Todos"),
     path("team/", views.team, name="Team"),
-    path("about/", views.about, name="About"),
 ]
